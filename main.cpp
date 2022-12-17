8a50b3589497d4010f084685e16279f4bc8f2423
SimoneCastorina <115416896+SimoneCastorina@users.noreply.github.com>
2022-12-17 11:42:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char list[10][20];
+    char name[20];
+    int p = 0;
+
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++) {
+            name[j] = ' ';
+            list[i][j] = ' ';
+        }
+    }
+    for(int i=0;i<10;i++){
+        cin >> list[i];
+    }
+
+    cin >> name;
+    if(comp(list,p,name)){
+        cout << p;
+    }else{
+        cout << "non presente";
+    }
+    return 0;
 }
