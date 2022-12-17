85e8a5e3e0424188435b23bc106ac910cbd9bad7
SimoneCastorina <115416896+SimoneCastorina@users.noreply.github.com>
2022-12-17 11:40:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
 
+bool comp(char list[10][20], int &p, char name[]){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++){
+            if(list[i][j] != name[j]){
+                j = 20;
+            }else{
+              if(j==19){
+                  p = i;
+                  return true;
+              }
+            }
+        }
+    }
+    return false;
+}
